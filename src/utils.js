ca1f89300836b0e1cab736dea8262fcdf50a8ef2
Fabrizio Ruggeri <fabrizio.ruggeri@gmail.com>
2019-01-08 17:31:45 +0100

    Smarter streamAsPromise

--- src/utils.js
+++ src/utils.js
@@ -1,12 +1,33 @@
+const { finished: originalFinished } = require('stream');
+const { promisify } = require('util');
 const fromValues = require('./fromValues');
 
+const finished = promisify(originalFinished);
+
 const utils = {
-  streamAsPromise: stream => new Promise((resolve, reject) => {
+  /**
+   * Return a promise from a stream
+   * @params {Object} options
+   * @params {Boolean} options.getData If true the promise will resolve with the data. Default true
+   * @params {Boolean} options.drain   If true and if possible the stream will be drained calling
+   *                                   "resume" on it. This let the stream complete even if no data
+   *                                   is collected. Default true
+   */
+  streamAsPromise: async (
+    stream,
+    { getData = true, drain = true } = { getData: true, drain: true },
+  ) => {
     const chunks = [];
-    stream.on('data', (data) => { chunks.push(data); });
-    stream.on('end', () => resolve(chunks));
-    stream.on('error', reject);
-  }),
+    if (getData) {
+      stream.on('data', (data) => { chunks.push(data); });
+    }
+    const promise = finished(stream);
+    if (drain && stream.resume) {
+      stream.resume();
+    }
+    await promise;
+    return chunks;
+  },
 
   createStream: fromValues,
 
